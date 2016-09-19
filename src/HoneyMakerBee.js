c08e5874012d8a4cab779c5b6b4c726b85c9ebae
rdgifford <robbie.gifford@gmail.com>
2016-09-19 11:07:55 -0700

    Finish beesbeesbees with passing tests for Bee, ForagerBee, Grub, HoneyMakerBee, and RetiredForager.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.subtractHoney = function() {
+  this.honeyPot--;
+}
+
+HoneyMakerBee.prototype.giveHoney = function(bee) {
+  this.honeyPot--;
+} 
